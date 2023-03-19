aa54b411b526831d3dbac412a03981b7446d56ed
Lewis Alderman <lewisalderman95@gmail.com>
2023-03-19 14:52:17 +0000

    create vite

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,4 +1,4 @@
-import { StackContext, Api } from "sst/constructs";
+import { StackContext, Api, StaticSite } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
   const api = new Api(stack, "api", {
@@ -6,7 +6,18 @@ export function API({ stack }: StackContext) {
       "GET /": "packages/functions/src/lambda.handler",
     },
   });
+
+  const site = new StaticSite(stack, "Site", {
+    path: "packages/site",
+    buildCommand: "yarn build",
+    buildOutput: "dist",
+    environment: {
+      VITE_API_URL: api.url,
+    },
+  });
+
   stack.addOutputs({
     ApiEndpoint: api.url,
+    SiteUrl: "" + site?.url,
   });
 }
